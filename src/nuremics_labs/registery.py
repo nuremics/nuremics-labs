a71eed499e55f13a1f779fbe8579f77736bc8b34
Julien Sigüenza <contact@julien-siguenza.com>
2026-04-03 17:47:33 +0200

    first working implementation

--- src/nuremics_labs/registery.py
+++
@@ -1,9 +0,0 @@
-from nuremics_labs.procs.general.PolygonGeometryProc import PolygonGeometryProc
-from nuremics_labs.procs.general.ProjectileModelProc import ProjectileModelProc
-from nuremics_labs.procs.general.TrajectoryAnalysisProc import TrajectoryAnalysisProc
-
-PROCS_REGISTRY = {
-    "PolygonGeometryProc": PolygonGeometryProc,
-    "ProjectileModelProc": ProjectileModelProc,
-    "TrajectoryAnalysisProc": TrajectoryAnalysisProc,
-}
